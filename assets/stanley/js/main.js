68a5fab7649c4cb472ca4c3d30d03106a7e03da7
Halisson Vitor <halissonvit@gmail.com>
2015-04-12 04:41:46 -0300

    adding sticky navbar on scroll

--- assets/stanley/js/main.js
+++ assets/stanley/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
     $('div.form-group.float-label-wrapper').floatLabel();
     $('img.swap-to-gif').swapToGif();
+
+    var properties = {
+        offset: '#ww',
+        clone: true,
+        throttle: 500,
+        onStick: function (headhesive) {
+            $.each([headhesive.elem, headhesive.targetElem], function(i, navbar) {
+                $(navbar).find('.avatar').removeClass('invisible');
+            });
+        },
+        onUnstick: function (headhesive) {
+            $.each([headhesive.elem, headhesive.targetElem], function(i, navbar) {
+                $(navbar).find('.avatar').addClass('invisible');
+            });
+        }
+
+    };
+    var headhesive = new Headhesive('.navbar', properties);
 });
--- assets/stanley/js/smoothscroll.js
+++ assets/stanley/js/smoothscroll.js
@@ -2,7 +2,7 @@
  * Credits: https://css-tricks.com/snippets/jquery/smooth-scrolling/
  */
 $(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
+    $('body').on('click', 'a[href*=#]:not([href=#])', function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
