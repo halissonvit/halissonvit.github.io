9de79e1c9b19a159def2f1ef6b8838e44f5a81af
Halisson Vitor <halissonvit@gmail.com>
2015-04-11 23:45:18 -0300

    bumping swap-to-gif version

--- assets/shared/js/jquery.swap-to-gif.js
+++ assets/shared/js/jquery.swap-to-gif.js
@@ -1,15 +1,17 @@
 (function ($) {
 
     $.fn.swapToGif = function () {
-        var staticImg = this.attr('src');
-        var gifImg = this.data('gif-src') ? this.data('gif-src') : staticImg.substr(0, staticImg.lastIndexOf(".")) + ".gif";
+        this.each(function () {
+            var staticImg = $(this).attr('src');
+            var gifImg = $(this).data('gif-src') ? $(this).data('gif-src') : staticImg.substr(0, staticImg.lastIndexOf(".")) + ".gif";
 
-        this.hover(function () {
-            $(this).attr('src', gifImg);
+            $(this).hover(function () {
+                $(this).attr('src', gifImg);
 
-        }, function () {
-            // on mouse leave
-            $(this).attr('src', staticImg);
+            }, function () {
+                // on mouse leave
+                $(this).attr('src', staticImg);
+            });
         });
     };
 })(jQuery);
